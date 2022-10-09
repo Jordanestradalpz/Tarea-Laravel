a7e1df11e92e7eacb6c6824d2a006d112acabc93
Jordan Estrada <estradajordan99@gmail.com>
2022-10-09 14:29:04 -0600

    Added Routes & Try Postman

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ZapatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/zapatos',[ZapatoController::class, 'index']);
+Route::post('/zapatos',[ZapatoController::class, 'store']);
+Route::put('/zapatos{id}',[ZapatoController::class, 'update']);
+Route::delete('/zapatos{id}',[ZapatoController::class, 'destroy']);
