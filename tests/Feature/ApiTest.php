45db1d6d007290824fccd375bbe0dfb137c118a8
Jordan Estrada <estradajordan99@gmail.com>
2022-10-13 20:49:57 -0600

    Added unit test

---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+
+    /** @test */
+
+    public function EntryCorrectFormatDescriptionTest()
+    {
+        $shoe = "shoeDescription";
+        $description = "shoeDescription";
+
+        $this->assertSame($shoe, $description);
+    
+    }
+
+    /** @test */
+
+    public function ConfirmDataEnteredTest()
+    {
+        $description1 = "Jordan 1 Retro High Dior";
+
+        $this->assertSame($description1, "Jordan 1 Retro High Dior");
+    }
+
+    
+}
